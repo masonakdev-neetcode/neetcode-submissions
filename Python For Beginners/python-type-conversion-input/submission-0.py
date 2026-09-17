041c24e29a6aa82566126806dff59340b03c3787
masonakdev-neetcode <neetcode-github@masoncamara.com>
2026-09-17 16:40:46 -0600

    Add: python-type-conversion-input - submission-0

---
+++ Python For Beginners/python-type-conversion-input/submission-0.py
@@ -0,0 +1,15 @@
+def read_integer() -> int:
+    return int(input())
+
+
+def read_float() -> float:
+    return float(input())
+
+
+print(read_integer())
+print(read_integer())
+print(read_integer())
+
+print(read_float())
+print(read_float())
+print(read_float())
