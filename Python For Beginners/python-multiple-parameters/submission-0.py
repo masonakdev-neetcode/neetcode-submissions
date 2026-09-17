dbf8e2c415cfc4b0c50a0567a0801f59295c691f
masonakdev-neetcode <neetcode-github@masoncamara.com>
2026-09-17 14:31:13 -0600

    Add: python-multiple-parameters - submission-0

---
+++ Python For Beginners/python-multiple-parameters/submission-0.py
@@ -0,0 +1,10 @@
+def two_sum(x: int, y: int):
+    print(x + y)
+
+def three_sum(x: int, y: int, z: int):
+    print(x + y + z)
+
+two_sum(7, 10)
+three_sum(3, 5, 8)
+two_sum(10, 9)
+three_sum(5, 14, 6)
