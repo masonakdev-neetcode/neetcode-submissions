37db6a22e59c81d950d27bed1e0c890a98ba576f
masonakdev-neetcode <neetcode-github@masoncamara.com>
2026-09-17 15:13:36 -0600

    Add: python-list-operations - submission-0

---
+++ Python For Beginners/python-list-operations/submission-0.py
@@ -0,0 +1,18 @@
+def check_list_empty(my_list) -> bool:
+    return len(my_list) <= 0
+
+def check_element_in_list(my_list, element) -> bool:
+    for elem in my_list:
+        if elem == element:
+            return True
+
+    return False
+
+print(check_list_empty([]))
+print(check_list_empty([1, 2, 3]))
+
+print(check_element_in_list([1, 2, 3], 1))
+print(check_element_in_list([1, 2, 3], 4))
+
+print(check_element_in_list(["Apple", "Banana", "Orange"], "Banana"))
+print(check_element_in_list(["Apple", "Banana", "Orange"], "Grape"))
