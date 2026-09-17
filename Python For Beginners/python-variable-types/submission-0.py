08562b1f94d280549de6a462f206aea0f5fbdaad
masonakdev-neetcode <neetcode-github@masoncamara.com>
2026-09-17 14:16:53 -0600

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 69
+float_type = 420.69
+bool_type = False
+string_type = "ur mom"
+list_type = ["ur", "mom", "is", "kinda", "bad"]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
