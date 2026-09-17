432e191ceb9deabffdf0f8286023ba7a27eb0955
masonakdev-neetcode <neetcode-github@masoncamara.com>
2026-09-17 14:50:45 -0600

    Add: python-if-scope - submission-0

---
+++ Python For Beginners/python-if-scope/submission-0.py
@@ -0,0 +1,8 @@
+def pay_bill(balance: int, bill: int) -> int:
+    if balance >= bill:
+        return balance - bill
+    return balance
+
+print(pay_bill(100, 50))
+print(pay_bill(100, 100))
+print(pay_bill(100, 150))
