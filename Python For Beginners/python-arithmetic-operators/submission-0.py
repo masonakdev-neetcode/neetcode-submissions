f3b5333fcc3d23830915cd6bfcfb036288bea7b4
masonakdev-neetcode <neetcode-github@masoncamara.com>
2026-09-17 14:21:12 -0600

    Add: python-arithmetic-operators - submission-0

---
+++ Python For Beginners/python-arithmetic-operators/submission-0.py
@@ -0,0 +1,10 @@
+a, b, c = 2, 2, 0.5
+
+sum_ = a + b + c
+product = a * b * c
+sum_divided_by_product = sum_ / product
+
+print(sum_)
+print (0 - sum_)
+print(product)
+print(sum_divided_by_product)
