7215d550112bc0ba3f688a44270ba757928b4cc1
masonakdev-neetcode <neetcode-github@masoncamara.com>
2026-09-17 14:17:47 -0600

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 69
+print(type(variable))
+
+variable = 420.69
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+variable = "ur mom"
+print(type(variable))
+
+variable = ["ur", "mom", "is", "kinda", "bad", "bro"]
+print(type(variable))
