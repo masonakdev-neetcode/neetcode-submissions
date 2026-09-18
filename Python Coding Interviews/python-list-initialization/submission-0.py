12edf89b3bb31f297fe291514364b64858a52f3d
masonakdev-neetcode <neetcode-github@masoncamara.com>
2026-09-17 21:34:05 -0600

    Add: python-list-initialization - submission-0

---
+++ Python Coding Interviews/python-list-initialization/submission-0.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def create_list_with_value(size: int, index: int, value: int) -> List[int]:
+    arr = [0] * size
+    arr[index] = value
+    return arr
+
+
+print(create_list_with_value(5, 3, 7))
+print(create_list_with_value(1, 0, 5))
+print(create_list_with_value(10, 9, 9))
+print(create_list_with_value(10, 9, 0))
