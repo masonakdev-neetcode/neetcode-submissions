c5295f3dbfd5a91d9d41ebaa54c7cd9a60561286
masonakdev-neetcode <neetcode-github@masoncamara.com>
2026-09-17 21:43:17 -0600

    Add: python-double-ended-queue - submission-0

---
+++ Python Coding Interviews/python-double-ended-queue/submission-0.py
@@ -0,0 +1,21 @@
+from typing import List, Deque
+from collections import deque
+
+
+def rotate_list(arr: List[int], k: int) -> Deque[int]:
+    n = len(arr)
+    res = list()
+    for i in range(n - k, n):
+        res.append(arr[i])
+    for i in range(0, n - k):
+        res.append(arr[i])
+    return deque(res)
+
+
+# do not modify below this line
+print(rotate_list([1, 2, 3, 4, 5], 0))
+print(rotate_list([1, 2, 3, 4, 5], 1))
+print(rotate_list([1, 2, 3, 4, 5], 2))
+print(rotate_list([1, 2, 3, 4, 5], 3))
+print(rotate_list([1, 2, 3, 4, 5], 4))
+print(rotate_list([1, 2, 3, 4, 5], 5))
