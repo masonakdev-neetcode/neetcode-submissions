fd89d8a23d22d634df0605c18cf8d643a16c644c
masonakdev-neetcode <neetcode-github@masoncamara.com>
2026-09-17 21:38:53 -0600

    Add: python-stack-push-pop - submission-0

---
+++ Python Coding Interviews/python-stack-push-pop/submission-0.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+def reverse_list(arr: List[int]) -> List[int]:
+    res = list()
+    for i in range(len(arr) - 1, -1, -1):
+        res.append(arr[i])
+    return res
+
+
+# do not modify below this line
+print(reverse_list([1, 2, 3]))
+print(reverse_list([3, 2, 1, 4, 6, 2]))
+print(reverse_list([1, 9, 7, 3, 2, 1, 4, 6, 2]))
