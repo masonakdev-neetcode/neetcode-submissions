9d7ad295191ea8b82d8bf382e03481b352137f2d
masonakdev-neetcode <neetcode-github@masoncamara.com>
2026-09-17 18:15:29 -0600

    Add: python-static-methods - submission-0

---
+++ Python OOP/python-static-methods/submission-0.py
@@ -0,0 +1,16 @@
+class CurrencyConverter:
+    rates = {  
+        'EUR': 1.20,  # 1 EUR = 1.20 USD
+        'JPY': 0.01   # 1 JPY = 0.01 USD
+    }
+
+    @staticmethod
+    def to_usd(amount: int, currency_code: str) -> float:
+        if currency_code in CurrencyConverter.rates:
+            return amount * CurrencyConverter.rates[currency_code]
+        
+        raise ValueError("Unrecognized currency code.")
+
+
+print(f"100 EUR = {CurrencyConverter.to_usd(100, 'EUR')} USD")     # 120 USD
+print(f"100 JPY = {CurrencyConverter.to_usd(100, 'JPY')} USD")     # 1 USD
