1c91889621489e8f36b08523e4c66ddf5ff158d4
masonakdev-neetcode <neetcode-github@masoncamara.com>
2026-09-18 09:15:44 -0600

    Add: python-custom-heap - submission-0

---
+++ Python Coding Interviews/python-custom-heap/submission-0.py
@@ -0,0 +1,17 @@
+import heapq
+from typing import List
+
+
+def get_reverse_sorted(nums: List[int]) -> List[int]:
+    max_heap = list()
+    arr = list()
+    for _, num in enumerate(nums):
+        heapq.heappush(max_heap, -num)
+    while max_heap:
+        arr.append(-heapq.heappop(max_heap))
+    return arr
+
+
+print(get_reverse_sorted([1, 2, 3]))
+print(get_reverse_sorted([5, 6, 4, 2, 7, 3, 1]))
+print(get_reverse_sorted([5, 6, -4, 2, 4, 7, -3, -1]))
