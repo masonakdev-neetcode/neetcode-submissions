e26b81af164446cbdec68f4140b00d226ef86136
masonakdev-neetcode <neetcode-github@masoncamara.com>
2026-09-20 22:51:49 -0600

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.py
@@ -0,0 +1,21 @@
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        # range(len(nums) + 1) because if nums=[7, 7] then count[2]=7, we need freq[2] in bounds
+        freq = [[] for i in range(len(nums) + 1)]
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+        
+        ans = []
+        # this can stop at index 1 because index 0 will never be used
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                ans.append(num)
+                if len(ans) == k:
+                    return ans
+
+        return [-69]
